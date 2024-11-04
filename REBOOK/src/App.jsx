65f1a39b2fbbb29ce521bf6a2111f4452a899e02
IanDal <ianjohndal5@gmail.com>
2024-11-05 00:26:25 +0800

    Update App.jsx
    Added Notif Routes

--- REBOOK/src/App.jsx
+++ REBOOK/src/App.jsx
@@ -7,7 +7,7 @@ import BookDetails from './components/BookDetails'; // Import the BookDetails co
 import User from './components/User'; // Import User component
 import LandingLayout from './components/Layout/LandingLayout'; // Ensure LandingLayout is properly imported
 import Category from './components/Category'; // Import the Category component
-
+import Notif from './components/Notif';
 function App() {
   return (
     <Router>
@@ -19,6 +19,7 @@ function App() {
           <Route path="books/:bookTitle" element={<BookDetails />} />
           <Route path="User" element={<User />} />
           <Route path="category/:categoryName" element={<Category />} /> {/* New route for categories */}
+          <Route path="Notif" element={<Notif />} />
         </Route>
       </Routes>
     </Router>
